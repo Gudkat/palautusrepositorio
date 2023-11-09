1bed8ab123059a8cd351c91543e97381bdf3a1e5
Timo Paloneva <timo.paloneva@gmail.com>
2023-11-09 19:46:52 +0200

    Refactored index.py. Solution for exercise 12 for week 2

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -2,24 +2,36 @@ import requests
 from player import Player
 
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+        self.players = []
+        self.get_json_data()
+
+    def get_json_data(self):
+        self.response = requests.get(self.url).json()
+
+    def get_players(self):
+        return [Player(player_dict) for player_dict in self.response]
+
+
+class PlayerStats:
+    def __init__(self, players: PlayerReader):
+        self.players = players.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        players = [
+            player for player in self.players if player.nationality == nationality]
+        return sorted(players, key=lambda pl: (pl.points(), pl.goals), reverse=True)
+
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    # Kommentoitu JSON printit pois. Oletan ettei niitä kukaan halua lukea.
-    # print("JSON-muotoinen vastaus:")
-    # print(response)
-
-    players = []
-
-    for player_dict in response:
-        if player_dict['nationality'] == "FIN":
-            player = Player(player_dict)
-            players.append(player)
-
-    print("Players from FIN:\n")
-
-    for player in sorted(players, key=lambda pl: (pl.points(), pl.goals), reverse=True):
+    for player in players:
         print(player)
 
 
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -4,9 +4,10 @@ class Player:
         self.goals = dict['goals']
         self.assists = dict['assists']
         self.team = dict['team']
+        self.nationality = dict['nationality']
 
     def points(self):
         return self.goals + self.assists
 
     def __str__(self):
-        return f"{self.name:20} {self.team:3} {self.goals:2} + {self.assists:2} = {self.points()}"
+        return f"{self.name:25} {self.team:3} {self.goals:2} + {self.assists:2} = {self.points()}"
