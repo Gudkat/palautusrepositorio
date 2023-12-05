c5dd0326040134c025ecbe0550f8d8262683ce77
Timo Paloneva <timo.paloneva@gmail.com>
2023-12-05 19:39:27 +0200

    added submission for week6 exercise 2

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,7 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
@@ -12,9 +13,23 @@ def main():
         HasAtLeast(20, "assists"),
         PlaysIn("PHI")
     )
+    matcher2 = And(
+        Not(HasAtLeast(2, "goals")),
+        PlaysIn("NYR")
+    )
 
-    for player in stats.matches(matcher):
+    matcher3 = And(
+        HasFewerThan(2, "goals"),
+        PlaysIn("NYR")
+    )
+
+    assert (stats.matches(matcher2) == stats.matches(matcher3))
+
+    for player in stats.matches(matcher2):
         print(player)
+
+    filtered_with_all = stats.matches(All())
+    assert (len(filtered_with_all) == 1058)
 
 
 if __name__ == "__main__":
