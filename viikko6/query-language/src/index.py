aefcf2b6db2466fbd8b440ca8dd272cd53edb681
Timo Paloneva <timo.paloneva@gmail.com>
2023-12-10 15:57:06 +0200

    added submisison to week6 exercise 4 and 5

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,47 +1,30 @@
 from statistics import Statistics
 from player_reader import PlayerReader
 from matchers import *
+from query_builder import QueryBuilder
 
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
+    url = "https://studies.cs.helsinki.fi//nhlstats/2022-23/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
-    )
-    matcher2 = And(
-        Not(HasAtLeast(2, "goals")),
-        PlaysIn("NYR")
-    )
+    query = QueryBuilder()
 
-    matcher3 = And(
-        HasFewerThan(2, "goals"),
-        PlaysIn("NYR")
-    )
-
-    assert (stats.matches(matcher2) == stats.matches(matcher3))
-
-    for player in stats.matches(matcher2):
-        print(player)
-
-    filtered_with_all = stats.matches(All())
-    assert (len(filtered_with_all) == 1058)
-
-    matcher = And(
-        HasAtLeast(70, "points"),
-        Or(
-            PlaysIn("NYR"),
-            PlaysIn("FLA"),
-            PlaysIn("BOS")
+    matcher = (
+        query
+        .oneOf(
+            query.playsIn("PHI")
+            .hasAtLeast(5, "assists")
+            .hasFewerThan(5, "goals")
+            .build(),
+            query.playsIn("EDM")
+            .hasAtLeast(5, "points")
+            .build()
         )
+        .build()
     )
 
-
-    print("\nOR matcher:")
     for player in stats.matches(matcher):
         print(player)
 
